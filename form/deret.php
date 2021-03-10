3e7830e39abe8b4ac26f0cf562ea2962e08aaa32
sukast69 <58833056+sukast69@users.noreply.github.com>
2021-03-10 21:38:14 +0800

    Add files via upload

--- form/deret.php
+++ form/deret.php
@@ -32,7 +32,8 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="../index.php"><img src="../asset/img/logo.png"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false">
                     <i class="fa fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
@@ -55,7 +56,8 @@
                             <a class="nav-link" href="#">CONTACT US</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 OTHERS
                             </a>
                             <div class="dropdown-menu active-home" aria-labelledby="navbarDropdown">
@@ -101,15 +103,15 @@
                 $a2 = $_GET['angka2'];
                 $deret = $_GET['deret'];
             ?>
-                <div class="result-containers">
-                    <div class="fibonancis-result">
-                        <div class="fibonancis-results">
-                            <h5>Hasilnya : </h5>
-                            
-                            <span class="result"><?php echo "$a1 $a2" ?></span>
+            <div class="result-containers">
+                <div class="fibonancis-result">
+                    <div class="fibonancis-results">
+                        <h5>Hasilnya : </h5>
 
-                            <?php
-                            for ($i = 1; $i <= $deret; $i++) {
+                        <span class="result"><?php echo "$a1 $a2" ?></span>
+
+                        <?php
+                            for ($i = 0; $i <= $deret - 2; $i++) {
 
                                 $hasil = $a2 + $a1;
 
@@ -119,13 +121,13 @@
                                 $a2 = $hasil;
 
                             ?>
-                            <?php
+                        <?php
                             } ?>
-                        </div>
-                        
                     </div>
-                   
+
                 </div>
+
+            </div>
             <?php
             }
             ?>
